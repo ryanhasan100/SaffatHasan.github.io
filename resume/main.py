2cb2853ebd229df0333a60dede91bb32a156ac1a
Saffat Hasan <saffathasan@gmail.com>
2020-09-11 18:26:58 -0400

    feat: templatize resume

--- resume/main.py
+++ resume/main.py
@@ -14,13 +14,13 @@ def main():
 
 def get_template(template_file):
     latex_jinja_env = jinja2.Environment(
-    	# block_start_string = '\BLOCK{',
-    	# block_end_string = '}',
+    	block_start_string = '\BLOCK{',
+    	block_end_string = '}',
     	variable_start_string = '\VAR{',
     	variable_end_string = '}',
     	# comment_start_string = '\#{',
     	# comment_end_string = '}',
-    	# line_statement_prefix = '%%',
+    	line_statement_prefix = '%%',
     	# line_comment_prefix = '%#',
     	# trim_blocks = True,
     	# autoescape = False,
